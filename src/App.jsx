242180fb1bc411329438ab93baad37219a8c3512
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-04-16 21:11:27 +0600

    show pricing card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DaisyNav from './Components/DaisyUi/DaisyNav'
 import Nav from './Components/Navbar/Nav'
+import PricingOption from './Components/PricingOptions/PricingOption'
+
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav></Nav>
+      <header>
+        <Nav></Nav>
+      </header>
+
+      <main>
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <PricingOption pricingPromise={pricingPromise}></PricingOption>
+        </Suspense>
+      </main>
     </>
   )
 }
--- src/Components/PricingOptions/PricingOption.jsx
+++ src/Components/PricingOptions/PricingOption.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {use} from 'react';
+import PricingCard from '../pricingCard/PricingCard';
 
-function PricingOption() {
+function PricingOption({pricingPromise}) {
+
+    const pricingData = use(pricingPromise);
+
   return (
     <div>
-      
+      <h2 className='text-5xl'>Get our Membership</h2>
+      <div className='grid md:grid-cols-3 gap-5'>
+        {
+          pricingData.map(pricing => <PricingCard 
+            key={pricing.id} 
+            pricing={pricing}></PricingCard>)
+        }
+      </div>
     </div>
   );
 }
